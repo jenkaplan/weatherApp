270847cb79c50c786bcbc3ac7e2d85f65a53536c
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-02-12 14:47:33 -0500

    location console logging

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,29 @@ require('dotenv').config();
 router.get('/', function(req, res, next) {
   axios.get(`https://api.darksky.net/forecast/${process.env.weatherKey}/42.3601,-71.0589`)
   .then((response) => {
+    // console.log('darksky response', response)
+    // console.log('darksky response')
     const temp = Math.round(response.data.currently.temperature);
     const summary = response.data.currently.summary;
+    // const response = response;
 
     res.render('index', {
       temp: temp,
       summary: summary,
+      response: response
+    });
+    // console.log('darksky response')
+  });
+
+  axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.googleLocationKey}`)
+  .then((response) => {
+    console.log('google response', response)
+    const lat = response.location.lat;
+    const lng = response.location.lng;
+
+    res.render('index', {
+      lat: lat,
+      lng: lng
     });
   });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+require('dotenv').config();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  axios.get(`https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.googleLocationKey}`)
+  .then((response) => {
+    const lat = response.location.lat;
+    const lng = response.location.lng;
+
+    res.render('index', {
+      lat: lat,
+      lng: lng
+    })
+  })
 });
 
 module.exports = router;
