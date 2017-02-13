66dd0f51cbc3a83c3dec720b4528fadf0a5d8946
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-02-12 22:48:59 -0500

    cleaned up code

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
-const location = require('./routes/location');
 const env = require('dotenv').config();
 
 const app = express();
@@ -30,7 +29,6 @@ app.use(require('node-sass-middleware')({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/location', location);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 require('dotenv').config();
-const getLocation = require('./location');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,25 +21,6 @@ router.get('/', function(req, res, next) {
     });
   })
   .catch((err) => console.log(err))
-  //res.send();
-  //getLocation().then(val => {console.log(val)});
-  /*
-
-  .then((val) => {
-    axios.get(`https://api.darksky.net/forecast/${process.env.weatherKey}/${location.lat},${location.lng}`)
-  .then((response) => {
-    const temp = Math.round(response.data.currently.temperature);
-    const summary = response.data.currently.summary;
-
-    res.render('index', {
-      temp: temp,
-      summary: summary,
-      response: response
-    });
-  })
-  .catch((err) => console.log(err))
-  })
-  */
 });
 
 module.exports = router;
