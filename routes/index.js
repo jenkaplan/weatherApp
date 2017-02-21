1ea3c0e85acc9fcf8c4f5fc65427f2e5e79043a3
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-02-20 20:42:56 -0500

    updated console log again

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.googleLocationKey}`)
-    console.log(data.location.lat, data.location.lng)
+    console.log(location.lat, location.lng)
   .then((val) => {
     return axios.get(`https://api.darksky.net/forecast/${process.env.weatherKey}/${val.data.location.lat},${val.data.location.lng}`)
   })
